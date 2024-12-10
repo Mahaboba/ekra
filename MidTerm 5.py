d1ad70660a4ed64e84a770176c213e426c7546d1
Mahaboba <jesminekra@gmail.com>
2024-12-10 18:46:53 +0600

    new

---
+++ MidTerm 5.py
@@ -0,0 +1,20 @@
+'''r=4
+area=3.1416*r*r
+print(area)
+
+list=[1,2,3,4,5,6,7]
+n=7
+sum=(n*(n+1))/2
+average= sum/n
+print(sum,average)
+
+first_name='Ansuya'
+input(first_name)
+last_name='Acharjee'
+input(last_name)
+'''
+print('Ansuya',end=' ')
+print('Acharjee')
+name='Ansuya Acharjee'
+T=name.upper()
+print(T)
